fb9a4b6c4d2a87a24d920947d1cc68e83e5572f1
Rynn <rynnschool12@gmail.com>
2025-05-06 23:19:30 +0700

    Matching 2 Device

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, accusantium?
+            </h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum, quidem voluptate? Recusandae, repellat tempore, corrupti quibusdam praesentium voluptatum aspernatur ad unde molestiae illo deserunt velit.</p>
+            <button>Sign Up</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
